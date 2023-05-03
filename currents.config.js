a7de83ef168a14ecd2faf3c1e35345ceab0552d2
Uma <umaram2694@gmail.com>
2023-05-03 12:49:16 +0530

    CMS_commit01

--- currents.config.js
+++ currents.config.js
@@ -1,6 +1,7 @@
 // currents.config.js
 module.exports = {
-    projectId: "Skylab-cms", // the projectId, can be any values for sorry-cypress users
+    projectId: "CMS", // the projectId, can be any values for sorry-cypress users
     recordKey: "xxx", // the record key, can be any value for sorry-cypress users
     cloudServiceUrl: "http://localhost:1234",   // Sorry Cypress users - set the director service URL
+    
   };
--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,19 @@
 
 const { defineConfig } = require("cypress");
 const { cloudPlugin } = require("cypress-cloud/plugin");
+
 module.exports = defineConfig({
   env: {
     BaseUrl: "https://cmsweb-new-qa2.skylab.world/",
     Dashboard: "https://cmsweb-new-qa2.skylab.world/home"
+    
   },
   
 
   e2e: {
     watchForFileChanges: false, //cltest will not run after immediate change in the script 
     defaultCommandTimeout: 5000, //execution will be delayed for mentioned millisecond if any failure occurs in the command 
+    videoUploadOnPasses: false,
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
       on('task', {
@@ -21,8 +24,8 @@ module.exports = defineConfig({
           return null// implement node event listeners here
         },
         specPattern: 'cypress/api-test/**.{js,jsx,ts,tsx}',
-        projectId: "Skylab-cms",
-        experimentalRunAllSpecs: true
+        projectId: "CMS",
+        experimentalRunAllSpecs: false
         
       })
       return cloudPlugin(on, config);
